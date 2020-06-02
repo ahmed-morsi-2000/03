dcb1aae5f6c76177c234bafe62ee2efc830c1522
morsi@uni-koblenz.de <morsi@uni-koblenz.de>
2020-06-02 02:17:46 +0200

    Caro1 class modified

--- Car01.java
+++ Car01.java
@@ -3,7 +3,7 @@ public class Car01 extends Auto{
 
     public Car01(int maxSpeed, String color) {
         super(maxSpeed, color);
-        System.out.println("You maximum speed is "+ maxSpeed + " " + color + " which I think cool");
+        System.out.println("You maximum speed is "+ super.getMaxSpeed() + " " + color + " which I think cool");
     }
 
     public static void main(String[] args) {
