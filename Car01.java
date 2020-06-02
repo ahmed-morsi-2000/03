d9975fc1b9c3863440bca7d59c535764d1730798
morsi@uni-koblenz.de <morsi@uni-koblenz.de>
2020-06-02 10:13:18 +0200

    Auto class with avaerage method

--- Car01.java
+++ Car01.java
@@ -7,7 +7,23 @@ public class Car01 extends Auto{
     }
 
     public static void main(String[] args) {
+        int []val= {10, 20, 30, 40};
         Car01 car = new Car01(120, "Red");
+        averageNumber(val);
+
+    }
+
+
+    public static void averageNumber(int[] values){
+        int count =0;
+        int average=0;
+
+        for (int i=0; i<values.length; i++){
+            count=count+values[i];
+           average=count/values.length;
+
+        }
+        System.out.println(average);
 
 
     }
